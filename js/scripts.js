71b612a2520d630e7983f5b8d8a1149c81a04ff2
Joseph Tomlinson and Scott Reichert <scott@>
2017-09-14 10:49:01 -0700

    add penguin and dolphin clickables

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#animal1").click(function() {
+    $("#animal-name").text("Dolphin")
+    $("#animal-description").text("This is a fantastic Dolphin!")
+  });
+
+
+  $("#animal2").click(function() {
+    $("#animal-name").text("Penguin")
+    $("#animal-description").text("This is a legit Penguin!")
+  });
+});
