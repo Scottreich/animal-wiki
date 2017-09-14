971e799369a0d561cd54c35fc9fbbc976a46fe09
Joseph Tomlinson and Scott Reichert <joseph@>
2017-09-14 11:09:35 -0700

    add images to wiki

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   $("#animal1").click(function() {
-    $("#animal-name").text("Dolphin")
-    $("#animal-description").text("This is a fantastic Dolphin!")
+    $("#animal-name").text("Dolphin");
+    $("#animal-description").text("This is a fantastic Dolphin!");
+    $("#animal-image").attr("src","img/dolphin.jpg");
+    $("#animal-image").show();
   });
 
 
   $("#animal2").click(function() {
-    $("#animal-name").text("Penguin")
-    $("#animal-description").text("This is a legit Penguin!")
+    $("#animal-name").text("Penguin");
+    $("#animal-description").text("This is a legit Penguin!");
+    $("#animal-image").attr("src", "img/penguin.jpg");
+    $("#animal-image").show();
   });
 });
